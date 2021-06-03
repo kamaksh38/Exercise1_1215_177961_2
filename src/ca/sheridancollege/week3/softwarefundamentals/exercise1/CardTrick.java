cb5f287ac8f5b22d71b1c309ad8efd6e84eae435
steevesm <84749845+steevesm@users.noreply.github.com>
2021-06-03 12:03:38 -0400

    First Commit - New Branch 991351367 steevesm

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -4,12 +4,13 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author steevesm
  */
 public class CardTrick {
     
@@ -19,14 +20,27 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
+            Random r =  new Random();
+            
+            int randSuit = r.nextInt(4);
+            int randValue =  r.nextInt(13)+1;
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(randValue);
+            c.setSuit(Card.SUITS[randSuit]);
+            magicHand[i] = c;
+           
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter in Card Value: ");
+        String inputValue = in.nextLine();
+        int userValue = Integer.parseInt(inputValue);
+        System.out.println("Enter in Card Suit: ");
+        String inputSuit = in.nextLine();
+        
     
+    }
 }
