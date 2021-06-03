f28b2613fb51d505f810d6781c4fe9887faa40d9
Annie <kinehy@sheridancollege.ca>
2021-06-03 13:31:58 -0400

    Fourth Commit (Date added) - EHYUN_KIM_Exercise1

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
- * @modifier E Hyun Kim
+ * @modifier E Hyun Kim 
+ * Date Modified: June 3, 2021
  * Student number: 991636054
  */
 public class CardTrick {
