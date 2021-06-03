f08a622a11dc16628e1f113cbb646df175f7b016
Annie <kinehy@sheridancollege.ca>
2021-06-03 11:15:55 -0400

    First Commit - EHYUN_KIM_Exercise1

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -4,28 +4,37 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.exercise1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier E Hyun Kim
+ * Student number: 991636054
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+                
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random() * 13) + 1); 
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
+        System.out.print("Pick a card (value & suit): ");
+        String pickedCard = input.nextLine();
+        
         //Then report the result here
     }
     
