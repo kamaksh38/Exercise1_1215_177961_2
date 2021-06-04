6260a4e150e98a50c3c735b4fe0956418d24ab7a
steevesm <84749845+steevesm@users.noreply.github.com>
2021-06-03 23:05:40 -0400

    Added comments and adjusted Javadoc

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
@@ -12,7 +12,9 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author steevesm
+ * @author dancye
+ * @modifiedBy Matthew Steeves
+ * Student Number: 991351367
  */
 public class Card {
 
--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -10,7 +10,9 @@ import java.util.Scanner;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author steevesm
+ * @author dancye
+ * @modifiedBy Matthew Steeves
+ * Student Number: 991351367
  */
 public class CardTrick {
     
@@ -22,7 +24,7 @@ public class CardTrick {
             Random r =  new Random();
             
             int randSuit = r.nextInt(4);
-            int randValue =  r.nextInt(13)+1;
+            int randValue =  r.nextInt(13)+1; //Need to use +1 to stop 0 from being an outcome
             Card c = new Card();
             c.setValue(randValue);
             c.setSuit(Card.SUITS[randSuit]);
@@ -30,18 +32,21 @@ public class CardTrick {
            
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        //Creating a lucky card and assigning it values
         Card luckyCard = new Card();
         luckyCard.setValue(10);
         luckyCard.setSuit("Hearts");
+        
+        /*For loop to check if the value and suit of the luckycard
+        matches any of the cards in the hand array*/
         int result = 0;
         for(int i=0;i<magicHand.length;i++){
             if((luckyCard.getValue() == magicHand[i].getValue()) && (luckyCard.getSuit() == magicHand[i].getSuit())){
                 result = 1;
             }
         }
+        
+        //Prints result based on outcome
         if(result == 1){
             System.out.println("You Won!");
         } else {
